0ea4c6cbc14df37d45ebde4ff5662e7590523c48
Kenan Rhoton <kenan.rhoton@gmail.com>
2018-01-28 09:53:29 +0100

    Fixed test for new regional indicator votes

--- src/test_votes.py
+++ src/test_votes.py
@@ -13,9 +13,9 @@ class TestVote(unittest.TestCase):
             message, error = votes.vote(*opt)
             self.assertIsNone(error)
             self.assertEqual(message, (f"{opt[0]}\n"
-                f":potato: {opt[1]}\n"
-                f":bacon: {opt[2]}\n"
-                f":alien: {opt[3]}"))
+                f":regional_indicator_a: {opt[1]}\n"
+                f":regional_indicator_b: {opt[2]}\n"
+                f":regional_indicator_c: {opt[3]}"))
 
     def test_VoteFewArgs(self):
         """Should return a usage message without enough args"""
